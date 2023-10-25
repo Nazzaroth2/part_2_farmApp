d875526269829af0d4b7108665affc9a62a0024f
Nazzaroth2 <cernyviktor@web.de>
2023-10-25 19:30:06 +0200

    inital frontend setup

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div className="App bg-red-500">
+       <h1 className="text-white text-
+         center">This is a Tailwind styled site!</h1>
+      </div>
+    );
+  }
+
+export default App;

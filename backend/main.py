b51b9dd331e7d188cd5d9a98d1ce0b508204f463
Nazzaroth2 <cernyviktor@web.de>
2023-10-25 12:49:46 +0200

    Pre-Heroku inital publication, final commit

--- backend/main.py
+++ backend/main.py
@@ -1,17 +1,23 @@
 from decouple import config
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from contextlib import asynccontextmanager
 
-
 from routers.cars import router as cars_router
 
 DB_URL = config("DB_URL", cast=str)
 DB_NAME = config("DB_NAME", cast=str)
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+    "http://localhost:8000",
+]
 
 
 # new fastapi system for startup and shutdown with lifespan
@@ -28,6 +34,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(cars_router, prefix="/cars", tags=["cars"])
